a54d15b8578636f934541c98f566381581f47a24
marcos <marfergar48@alu.edu.gva.es>
2025-12-05 08:44:22 +0100

    Practica Netbeans mas git

---
+++ HolaGit/src/main/java/com/mycompany/holagit/HolaGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holagit;
+
+/**
+ *
+ * @author 6DAWM
+ */
+public class HolaGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde GIT!Soy Marcos ");
+    }
+}
